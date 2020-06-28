21befdd8e61d76da01cf55106e56d6c0080bd319
Jari Vetoniemi <mailroxas@gmail.com>
2020-06-28 20:55:03 +0900

    personal: disable LUNARG layer

--- engine/renderer/vulkan/renderer.cpp
+++ engine/renderer/vulkan/renderer.cpp
@@ -18,7 +18,7 @@ namespace lotus
 
     const std::vector<const char*> validation_layers = {
         "VK_LAYER_KHRONOS_validation",
-        "VK_LAYER_LUNARG_monitor"
+//        "VK_LAYER_LUNARG_monitor"
     };
     #ifdef NDEBUG
     const bool enableValidationLayers = false;
